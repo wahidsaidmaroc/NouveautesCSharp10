d991c7dd5728c8d836bb77b2c94e7a0748baea4a
Said WAHID <said@oumdin.com>
2023-05-22 11:05:11 +0100

    UpdateFile

--- NouveautesCSharp10/DeconstructionClass/Program.cs
+++ NouveautesCSharp10/DeconstructionClass/Program.cs
@@ -1,12 +1,23 @@
 ﻿using DeconstructionClass;
 
+
+
+
+
 var obj = new Client();
 obj.idClient = 1;
 obj.nomClient = "Said";
 
+//C# 9
+//Erreur 
 var id = 3;
 
 (id, string nom) = obj;
+
+//C# 10
+var idNew = 3;
+
+(idNew, string nomNew) = obj;
 
 Console.WriteLine(id);
 Console.WriteLine(nom);
---
+++ NouveautesCSharp10/ExpressionWith/Program.cs
@@ -0,0 +1,13 @@
+﻿var person = new
+{
+    Age = 1,
+    Name = "Test"
+};
+
+
+var person2 = person with { Age = 2 };
+
+
+
+
+Console.Write(person2.Name);
